e36db79dc0be6d3204759b6500034f64cf257658
nikdelvin <the@nikdelv.in>
2025-07-13 18:33:19 +0400

    First podcast

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+import tailwindcss from '@tailwindcss/vite'
+
+import solidJs from '@astrojs/solid-js'
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nikdelvin/feelicy' }],
+            sidebar: [
+                {
+                    label: 'Podcasts',
+                    autogenerate: { directory: 'podcasts' }
+                }
+            ],
+            customCss: ['./src/styles/global.css']
+        }),
+        solidJs()
+    ],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    devToolbar: {
+        enabled: false
+    }
+})
